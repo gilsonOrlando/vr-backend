cdcfe3fe6b61142ad8d89e8185fe7ebb5746a205
gilsonOrlando <gilson.quezada@unl.edu.ec>
2024-09-10 11:10:31 -0500

    Defecto01_ClasePersonal- Solucióndefecto

--- app/controllers/personal.js
+++ app/controllers/personal.js
@@ -1,70 +1,62 @@
-const mongoose = require('mongoose')
-const model = require('../models/personal')
-const parseId = (id) => {
-    return mongoose.Types.ObjectId(id)
-}
-/**
- * Obtener DATA de USUARIOS
- */
-exports.getData = (req, res) => {
-    model.find({}, (err, docs) => {
-        res.send({
-            items: docs
-        })
-    })
-}
-/**
- * Obtener DATA de USUARIOS
- */
+const mongoose = require('mongoose');
+const model = require('../models/personal');
 
-exports.getSingle = (req, res) => {
-    model.findOne({ _id: parseId(req.params.id) },
-        (err, docs) => {
-            res.send({
-                items: docs
-            })
-        })
-}
-/**
- * Obtener DATA de USUARIOS
- */
+const parseId = (id) => mongoose.Types.ObjectId(id);
 
-exports.updateSingle = (req, res) => {
-    const { id } = req.params
-    const body = req.body
-    model.updateOne(
-        { _id: parseId(id) },
-        body,
-        (err, docs) => {
-            res.send({
-                items: docs
-            })
-        })
-}
-/**
- * Insertar DATA de USUARIOS
- */
-exports.insertData = (req, res) => {
-    const data = req.body
-    model.create(data, (err, docs) => {
-        if (err) {
-            res.status(422.).send({ error: 'Error' })
-        } else {
-            res.send({ data: docs })
+// Función auxiliar para manejar la respuesta
+const handleResponse = (res, err, docs, status = 200) => {
+    if (err) {
+        res.status(status).send({ error: 'Error en la operación' });
+    } else {
+        res.status(status).send({ items: docs });
+    }
+};
+
+// Función para ejecutar operaciones del modelo
+const executeModelOperation = async (operation, query, body = null) => {
+    try {
+        let result;
+        switch (operation) {
+            case 'find':
+                result = await model.find(query).exec();
+                break;
+            case 'findOne':
+                result = await model.findOne(query).exec();
+                break;
+            case 'updateOne':
+                result = await model.updateOne(query, body).exec();
+                break;
+            case 'create':
+                result = await model.create(body);
+                break;
+            case 'deleteOne':
+                result = await model.deleteOne(query).exec();
+                break;
+            default:
+                throw new Error('Operación no soportada');
         }
+        return result;
+    } catch (err) {
+        throw err;
+    }
+};
 
-    })
-}
-/**
- * Obtener DATA de USUARIOS
- */
-exports.deleteSingle = (req, res) => {
-    const { id } = req.params
-    model.deleteOne(
-        { _id: parseId(id) },
-        (err, docs) => {
-            res.send({
-                items: docs
-            })
-        })
-}
+// Controlador genérico
+const handleRequest = (operation, queryRequired, bodyExtractor) => async (req, res) => {
+    try {
+        const query = queryRequired ? { _id: parseId(req.params.id) } : {};
+        const body = bodyExtractor ? bodyExtractor(req) : null;
+        const result = await executeModelOperation(operation, query, body);
+        const status = operation === 'create' ? 201 : 200;
+        handleResponse(res, null, result, status);
+    } catch (err) {
+        handleResponse(res, err);
+    }
+};
+
+// Controladores específicos
+exports.getData = handleRequest('find', false, null);
+exports.getSingle = handleRequest('findOne', true, null);
+exports.updateSingle = handleRequest('updateOne', true, req => req.body);
+exports.insertData = handleRequest('create', false, req => req.body);
+exports.deleteSingle = handleRequest('deleteOne', true, null);
--- app/controllers/solicitud.js
+++ app/controllers/solicitud.js
@@ -1,72 +1,37 @@
-const mongoose = require('mongoose')
-const model = require('../models/solicitud')
-const options = {
-    page: 1,
-    limit: 3
-};
-const parseId = (id) => {
-    return mongoose.Types.ObjectId(id)
-}
-/**
- * Obtener DATA de USUARIOS
- */
-exports.getData = (req, res) => {
-    model.find({}, (err, docs) => {
-        res.send({
-            items: docs
-        })
-    })
-}
-/**
- * Obtener DATA de USUARIOS
- */
-exports.getSingle = (req, res) => {
-    model.findOne({ _id: parseId(req.params.id) },
-        (err, docs) => {
-            res.send({
-                items: docs
-            })
-        })
-}
-/**
- * Obtener DATA de USUARIOS
- */
-exports.updateSingle = (req, res) => {
-    const { id } = req.params
-    const body = req.body
-    model.updateOne(
-        { _id: parseId(id) },
-        body,
-        (err, docs) => {
-            res.send({
-                items: docs
-            })
-        })
-}
-/**
- * Insertar DATA de USUARIOS
- */
-exports.insertData = (req, res) => {
-    const data = req.body
-    model.create(data, (err, docs) => {
-        if (err) {
-            res.status(422.).send({ error: 'Error' })
-        } else {
-            res.send({ data: docs })
-        }
+const mongoose = require('mongoose');
+const model = require('../models/solicitud');
 
-    })
-}
-/**
- * Obtener DATA de USUARIOS
- */
+const parseId = (id) => mongoose.Types.ObjectId(id);
+
+// Función para manejar operaciones CRUD con error y respuesta
+const performOperation = (operation, query, res, body = null) => {
+    model[operation](query, body, (err, docs) => {
+        if (err) {
+            res.status(err.status || 500).send({ error: 'Error en la operación' });
+        } else {
+            res.send({ items: docs });
+        }
+    });
+};
+
+// Controladores específicos
+
+exports.getData = (req, res) => {
+    performOperation('find', {}, res);
+};
+
+exports.getSingle = (req, res) => {
+    performOperation('findOne', { _id: parseId(req.params.id) }, res);
+};
+
+exports.updateSingle = (req, res) => {
+    performOperation('updateOne', { _id: parseId(req.params.id) }, res, req.body);
+};
+
+exports.insertData = (req, res) => {
+    performOperation('create', null, res, req.body);
+};
+
 exports.deleteSingle = (req, res) => {
-    const { id } = req.params
-    model.deleteOne(
-        { _id: parseId(id) },
-        (err, docs) => {
-            res.send({
-                items: docs
-            })
-        })
-}
+    performOperation('deleteOne', { _id: parseId(req.params.id) }, res);
+};
