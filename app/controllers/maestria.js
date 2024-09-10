6c0a8714145426ea5fefab09ace99000779fece2
gilsonOrlando <gilson.quezada@unl.edu.ec>
2024-09-10 10:12:59 -0500

    Defecto01_ClaseMaestria- Solucióndefecto

--- app/controllers/maestria.js
+++ app/controllers/maestria.js
@@ -1,72 +1,75 @@
-const mongoose = require('mongoose')
-const model = require('../models/maestria')
-const options = {
-    page: 1,
-    limit: 3
+const mongoose = require('mongoose');
+const model = require('../models/maestria');
+
+// Función para parsear el ID de MongoDB
+const parseId = (id) => mongoose.Types.ObjectId(id);
+
+// Función genérica para manejar operaciones y respuestas
+const handleRequest = (operation, req, res, data = {}) => {
+    let query = data.query || {};
+    let update = data.update || {};
+
+    switch (operation) {
+        case 'find':
+            model.find(query, (err, docs) => sendResponse(res, err, docs));
+            break;
+        case 'findOne':
+            model.findOne(query, (err, docs) => sendResponse(res, err, docs));
+            break;
+        case 'updateOne':
+            model.updateOne(query, update, (err, docs) => sendResponse(res, err, docs));
+            break;
+        case 'create':
+            model.create(data.body, (err, docs) => sendResponse(res, err, docs));
+            break;
+        case 'deleteOne':
+            model.deleteOne(query, (err, docs) => sendResponse(res, err, docs));
+            break;
+        default:
+            res.status(400).send({ error: 'Operación no soportada' });
+    }
 };
-const parseId = (id) => {
-    return mongoose.Types.ObjectId(id)
-}
+
+// Función común para manejar respuestas y errores
+const sendResponse = (res, err, docs) => {
+    if (err) {
+        res.status(500).send({ error: 'Error en la operación' });
+    } else {
+        res.send({ items: docs });
+    }
+};
+
 /**
- * Obtener DATA de USUARIOS
+ * Obtener múltiples registros de USUARIOS
  */
 exports.getData = (req, res) => {
-    model.find({}, (err, docs) => {
-        res.send({
-            items: docs
-        })
-    })
-}
+    handleRequest('find', req, res);
+};
+
 /**
- * Obtener DATA de USUARIOS
+ * Obtener un solo registro de USUARIOS
  */
 exports.getSingle = (req, res) => {
-    model.findOne({ _id: parseId(req.params.id) },
-        (err, docs) => {
-            res.send({
-                items: docs
-            })
-        })
-}
+    handleRequest('findOne', req, res, { query: { _id: parseId(req.params.id) } });
+};
+
 /**
- * Obtener DATA de USUARIOS
+ * Actualizar un solo registro de USUARIOS
  */
 exports.updateSingle = (req, res) => {
-    const { id } = req.params
-    const body = req.body
-    model.updateOne(
-        { _id: parseId(id) },
-        body,
-        (err, docs) => {
-            res.send({
-                items: docs
-            })
-        })
-}
+    handleRequest('updateOne', req, res, { query: { _id: parseId(req.params.id) }, update: req.body });
+};
+
 /**
- * Insertar DATA de USUARIOS
+ * Insertar un nuevo registro de USUARIOS
  */
 exports.insertData = (req, res) => {
-    const data = req.body
-    model.create(data, (err, docs) => {
-        if (err) {
-            res.status(422.).send({ error: 'Error' })
-        } else {
-            res.send({ data: docs })
-        }
+    handleRequest('create', req, res, { body: req.body });
+};
 
-    })
-}
 /**
- * Obtener DATA de USUARIOS
+ * Eliminar un solo registro de USUARIOS
  */
 exports.deleteSingle = (req, res) => {
-    const { id } = req.params
-    model.deleteOne(
-        { _id: parseId(id) },
-        (err, docs) => {
-            res.send({
-                items: docs
-            })
-        })
-}
+    handleRequest('deleteOne', req, res, { query: { _id: parseId(req.params.id) } });
+};
