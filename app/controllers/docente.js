1277e6612605c4bdfa0ffb457db8836529a531b8
gilsonOrlando <gilson.quezada@unl.edu.ec>
2024-09-10 12:03:56 -0500

    Defecto01_ClaseDocente- Solucióndefecto

--- app/controllers/docente.js
+++ app/controllers/docente.js
@@ -1,76 +1,48 @@
-const mongoose = require('mongoose')
-const model = require('../models/docente')
+const mongoose = require('mongoose');
+const model = require('../models/docente');
 
-const parseId = (id) => {
-    return mongoose.Types.ObjectId(id)
-}
-/**
- * Obtener DATA de USUARIOS
- */
+// Función para manejar operaciones de base de datos
+const handleDatabaseOperation = async (operation, res, successStatus = 200) => {
+    try {
+        const result = await operation;
+        res.status(successStatus).send({ items: result });
+    } catch (err) {
+        console.error(err); // Registro de errores para depuración
+        res.status(422).send({ error: 'Error' });
+    }
+};
 
+// Función para convertir el ID a formato de MongoDB
+const parseId = id => mongoose.Types.ObjectId(id);
+
+// Función auxiliar para encontrar documentos
+const findDocuments = async (query) => model.find(query);
+const findOneDocument = async (query) => model.findOne(query);
+const updateDocument = async (query, update) => model.updateOne(query, update);
+const deleteDocument = async (query) => model.deleteOne(query);
+const createDocument = async (data) => model.create(data);
+
+// Controladores CRUD utilizando funciones auxiliares
 exports.getData = (req, res) => {
-    model.find({}, (err, docs) => {
-        res.send({
-            items: docs
-        })
-    })
-}
-
-/**
- * Obtener DATA de USUARIOS
- */
+    handleDatabaseOperation(findDocuments({}), res);
+};
 
 exports.getSingle = (req, res) => {
-    model.findOne({ _id: parseId(req.params.id) },
-        (err, docs) => {
-            res.send({
-                items: docs
-            })
-        })
-}
-
-/**
- * Obtener DATA de USUARIOS
- */
+    handleDatabaseOperation(findOneDocument({ _id: parseId(req.params.id) }), res);
+};
 
 exports.updateSingle = (req, res) => {
-    const { id } = req.params
-    const body = req.body
-    model.updateOne(
-        { _id: parseId(id) },
-        body,
-        (err, docs) => {
-            res.send({
-                items: docs
-            })
-        })
-}
+    const { id } = req.params;
+    const body = req.body;
+    handleDatabaseOperation(updateDocument({ _id: parseId(id) }, body), res);
+};
 
-
-//Guardar datos del docente en la base de datos
 exports.insertData = (req, res) => {
-    const data = req.body
-    model.create(data, (err, docs) => {
-        if (err) {
-            res.status(422.).send({ error: 'Error' })
-        } else {
-            res.send({ data: docs })
-        }
-
-    })
-}
-
-/**
- * Obtener DATA de USUARIOS
- */
+    const data = req.body;
+    handleDatabaseOperation(createDocument(data), res, 201);
+};
 
 exports.deleteSingle = (req, res) => {
-    const { id } = req.params
-    model.deleteOne(
-        { _id: parseId(id) },
-        (err, docs) => {
-            res.send({
-                items: docs
-            })
-        })
-}
+    const { id } = req.params;
+    handleDatabaseOperation(deleteDocument({ _id: parseId(id) }), res);
+};
