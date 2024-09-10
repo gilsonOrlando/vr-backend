c05a63d6818b1a43c2848a0bce418a6c053d61bd
gilsonOrlando <gilson.quezada@unl.edu.ec>
2024-09-10 08:54:54 -0500

    Defecto01_ClaseAudio- Solucióndefecto

--- app/controllers/audio.js
+++ app/controllers/audio.js
@@ -1,71 +1,72 @@
-const mongoose = require('mongoose')
-const model = require('../models/audio')
+const mongoose = require('mongoose');
+const model = require('../models/audio');
 const options = {
     page: 1,
     limit: 3
 };
+
 const parseId = (id) => {
-    return mongoose.Types.ObjectId(id)
+    return mongoose.Types.ObjectId(id);
 }
+
+// Función común para manejar la respuesta
+const sendResponse = (res, err, docs) => {
+    if (err) {
+        res.status(500).send({ error: 'Error en la operación' });
+    } else {
+        res.send({ items: docs });
+    }
+};
+
 /**
  * Obtener DATA de USUARIOS
  */
 exports.getData = (req, res) => {
     model.find({}, (err, docs) => {
-        res.send({
-            items: docs
-        })
-    })
-}
+        sendResponse(res, err, docs);
+    });
+};
+
 /**
- * Obtener DATA de USUARIOS
+ * Obtener un solo registro de USUARIOS
  */
 exports.getSingle = (req, res) => {
-    model.findOne({ _id: parseId(req.params.id) },
-        (err, docs) => {
-            res.send({
-                items: docs
-            })
-        })
-}
+    model.findOne({ _id: parseId(req.params.id) }, (err, docs) => {
+        sendResponse(res, err, docs);
+    });
+};
+
 /**
- * Obtener DATA de USUARIOS
+ * Actualizar un solo registro de USUARIOS
  */
 exports.updateSingle = (req, res) => {
-    const { id } = req.params
-    const body = req.body
-    model.updateOne(
-        { _id: parseId(id) },
-        body,
-        (err, docs) => {
-            res.send({
-                items: docs
-            })
-        })
-}
-//Guardar datos del audio en la base de datos
-exports.insertData = (req, res) => {
-    const data = req.body
-    model.create(data, (err, docs) => {
-        if (err) {
-            res.status(422.).send({ error: 'Error' })
-        } else {
-            res.send({ data: docs })
-        }
-
-    })
-}
+    const { id } = req.params;
+    const body = req.body;
+    model.updateOne({ _id: parseId(id) }, body, (err, docs) => {
+        sendResponse(res, err, docs);
+    });
+};
 
 /**
- * Obtener DATA de USUARIOS
+ * Insertar datos en la base de datos
+ */
+exports.insertData = (req, res) => {
+    const data = req.body;
+    model.create(data, (err, docs) => {
+        if (err) {
+            res.status(422).send({ error: 'Error al crear el registro' });
+        } else {
+            res.send({ data: docs });
+        }
+    });
+};
+
+/**
+ * Eliminar un solo registro de USUARIOS
  */
 exports.deleteSingle = (req, res) => {
-    const { id } = req.params
-    model.deleteOne(
-        { _id: parseId(id) },
-        (err, docs) => {
-            res.send({
-                items: docs
-            })
-        })
-}
+    const { id } = req.params;
+    model.deleteOne({ _id: parseId(id) }, (err, docs) => {
+        sendResponse(res, err, docs);
+    });
+};
