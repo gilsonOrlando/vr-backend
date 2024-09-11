ed71ce82b684009a7a157801bd843810b47cd73c
gilsonOrlando <gilson.quezada@unl.edu.ec>
2024-09-10 21:21:42 -0500

    Defecto03_ClaseDelete-Solucióndefecto

--- app/controllers/delete.js
+++ app/controllers/delete.js
@@ -1,34 +1,41 @@
-//función para eliminar archivos por su nombre
 const fs = require("fs");
+const path = require("path");
+
+// Función para eliminar archivos por su nombre (asíncrona)
 const remove = (req, res) => {
   const fileName = req.params.name;
-  const directoryPath = __basedir + "/uploads/";
-  fs.unlink(directoryPath + fileName, (err) => {
+  const directoryPath = path.join(__basedir, "uploads", fileName); // Usar path para mayor seguridad y consistencia
+
+  fs.unlink(directoryPath, (err) => {
     if (err) {
-      res.status(500).send({
-        message: "Could not delete the file. " + err,
+      return res.status(500).send({
+        message: "Could not delete the file. Error: " + err.message // Usar err.message para un error más claro
       });
     }
+
     res.status(200).send({
-      message: "File is deleted.",
+      message: "File is deleted."
     });
   });
 };
-//Este método bloquea el bucle de eventos de Node.js hasta que finaliza la acción.
+
+// Función para eliminar archivos de manera sincrónica (bloquea el evento loop)
 const removeSync = (req, res) => {
   const fileName = req.params.name;
-  const directoryPath = __basedir + "/uploads/";
+  const directoryPath = path.join(__basedir, "uploads", fileName); // Usar path para la ruta
+
   try {
-    fs.unlinkSync(directoryPath + fileName);
+    fs.unlinkSync(directoryPath);
     res.status(200).send({
-      message: "File is deleted.",
+      message: "File is deleted."
     });
   } catch (err) {
     res.status(500).send({
-      message: "Could not delete the file. " + err,
+      message: "Could not delete the file. Error: " + err.message // Usar err.message
     });
   }
 };
+
 module.exports = {
   remove,
   removeSync,
